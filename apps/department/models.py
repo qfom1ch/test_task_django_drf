e090e55a43717e7fdd0772aacd08584799148962
Сергей <viip222@yandex.ru>
2023-08-24 01:05:25 +0300

    first commit, create models

---
+++ apps/department/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Department(models.Model):
+    name = models.CharField('Название', max_length=150, unique=True)
+    director = models.ForeignKey('department.Department',
+                                 on_delete=models.DO_NOTHING,
+                                 related_name='departament',
+                                 null=True, blank=True,
+                                 verbose_name='Директор')
+
+    class Meta:
+        verbose_name = 'Департамент'
+        verbose_name_plural = 'Департаменты'
+        ordering = 'name',
+
+    def __str__(self):
+        return self.name
---
+++ apps/employee/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from django.utils.timezone import now
+
+
+class Employee(models.Model):
+    first_name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия', max_length=100, db_index=True)
+    surname = models.CharField('Отчество', max_length=100)
+    photo = models.ImageField('Фото', upload_to='media/users', blank=True)
+    job_title = models.CharField('Должность', max_length=100, blank=True)
+    salary = models.DecimalField('Оклад', default=0, max_digits=10, decimal_places=2)
+    birthday = models.DateField('День рождения', blank=True, null=True)
+    department = models.ForeignKey('employee.Employee', on_delete=models.CASCADE, related_name='employees',
+                                   verbose_name='Департамент')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+        ordering = 'last_name',
+        unique_together = ['id', 'department']
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.last_name} {self.first_name} {self.surname}'
+
+    @property
+    def age(self) -> int:
+        if self.birthday:
+            current_age = now().date().year - self.birthday.year
+            return current_age
+        return 0
+
+    def __str__(self) -> str:
+        return f'{self.full_name}, {self.job_title}'
